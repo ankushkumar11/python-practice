f1a935158e9037778da868e325ebeaa3e86b7f06
ankushkumar11 <ankush.kumar133@gmail.com>
2021-02-25 20:49:03 +0530

    Create fibo.py

---
+++ fibo.py
@@ -0,0 +1,9 @@
+# Fibonacci numbers module
+
+def fib(n):   # return Fibonacci series up to n
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
